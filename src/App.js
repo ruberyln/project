9e467171f6c7628c2f18985e65ab5c8fa69494d6
Jesutofunmi <tofunalabi@gmail.com>
2022-04-15 09:19:37 -0600

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World ! This is Tofunmi.
         </p>
+
+        <button> Click Here  </button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
