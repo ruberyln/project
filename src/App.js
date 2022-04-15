dfc71ccabf8a51b89a4932353b78dfd1c0aec896
Jesutofunmi <tofunalabi@gmail.com>
2022-04-15 14:08:34 -0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World ! This is Tofunmi.
-        </p>
-        <button> Click Here </button>
+     
+        <button type="button" className="btn btn-warning">Warning
+        </button>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
