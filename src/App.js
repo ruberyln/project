6cb15c5975f4c33f05cff818d13256d5f08dc217
Jesutofunmi <tofunalabi@gmail.com>
2022-04-15 09:24:58 -0600

    new

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-//import logo from './logo.svg';
-//import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World ! This is Tofunmi.
         </p>
-
-        <button> Click Here  </button>
+        <button> Click Here </button>
 
         <a
           className="App-link"
