76d5526de3aa0ed7980966fe714b12a3018b8a20
Jesutofunmi <tofunalabi@gmail.com>
2022-04-16 11:24:46 -0600

    new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-//import logo from './logo.svg';
+
 import './App.css';
 import Slider from "./Slider";
+import ImageSlider from './components/ImageSlider';
 function App() {
-  return (
-    <div className="App">
+  return<div className="App">
     
       <header className="App-header">
-      <Slider/>
+        <Slider/>
+
+      <ImageSlider />
+      
         <button type="button" className="btn btn-warning">Warning
         </button>
 
       </header>
     </div>
-  );
+  
 }
 
 export default App;
--- src/components/Sliderdata.js
+++ src/components/Sliderdata.js
@@ -1,8 +1,17 @@
-export const Sliderdata =[
+export const SliderData =[
     {
-    image: 'Cow Print Baguette Bag - Purple.jpg'
+    image: 'https://images.unsplash.com/photo-1557777586-f6682739fcf3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60'
     },
     {
-        image: 'Cow Print Baguette Bag - Purple.jpg'
-        }
+        image: 'https://images.unsplash.com/photo-1550614000-4895a10e1bfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60'
+            },
+            {
+                image: 'https://images.unsplash.com/photo-1539109136881-3be0616acf4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60'
+                },
+                {
+                    image: 'https://images.unsplash.com/photo-1601762603339-fd61e28b698a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60'
+                    } 
 ]
