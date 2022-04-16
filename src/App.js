3109820d126e9422dac150f092e8eddd62d50d59
Jesutofunmi <tofunalabi@gmail.com>
2022-04-16 10:08:18 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 //import logo from './logo.svg';
 import './App.css';
-
+import Slider from "./Slider";
 function App() {
   return (
     <div className="App">
+    <Slider/>
       <header className="App-header">
      
         <button type="button" className="btn btn-warning">Warning
---
+++ src/Slider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App.css';
+
+function Slider () {
+return (
+    <div className= "slider">
+       <p> WELCOME TO MY FIRST SITE </p>
+        
+        </div>
+
+);
+
+
+}
+export default Slider;
