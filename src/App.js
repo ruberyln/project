3c5f409b17b223c208a6405945d9dacb292db830
Jesutofunmi <tofunalabi@gmail.com>
2022-04-20 20:31:03 -0600

    new commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,57 @@ import Slider from "./Slider";
 import ImageSlider from './components/ImageSlider';
 import { SliderData } from './components/SliderData';
 function App() {
-  return <ImageSlider slides={SliderData}/>
+  return (
+    <><><><div className="App">
+      <header className="App-header">
+        <center><h1><b> RUBERYLN COUTURE</b> </h1> </center>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <img src="IMG_1444.JPG" alt="" width="30" height="24"></img>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.ruberyln.ecwid.com">SHOP</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">RENT-A-DRESS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CONTACT US</a>
+                </li>
+
+              </ul>
+              <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+
+            </div>
+          </div>
+        </nav>
+
+      </header>
+    </div><center><ImageSlider slides={SliderData} /></center></>
+      <p></p>
+      <center><button type="button" class="btn btn-dark">LAUNCHING SOON </button><p></p></center></>
       
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">EMAIL ADDRESS</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+            <div id="emailHelp" class="form-text">You will receive updates when we launch </div>
+            <center><button type="submit" class="btn btn-dark">Submit</button></center></div>
+ 
+      </form></>
+  )
       
   
 }
--- src/components/Sliderdata.js
+++ src/components/Sliderdata.js
@@ -1,17 +1,22 @@
 export const SliderData =[
     {
-    image: 'https://images.unsplash.com/photo-1533105079780-92b9be482077?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+        image: 'https://images.unsplash.com/photo-1603189343302-e603f7add05a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'
+    },
+    
+    {
+        image: 'https://images.unsplash.com/photo-1574015974293-817f0ebebb74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=673&q=80'
     },
     {
-        image: 'https://images.unsplash.com/photo-1528543606781-2f6e6857f318?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'
-        },
+    image: 'https://images.unsplash.com/flagged/photo-1570733117311-d990c3816c47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+},
+    
         {
-            image: 'https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1721&q=80'
-            },
+            image: 'https://images.unsplash.com/photo-1576193929684-06c6c6a8b582?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+         },
             {
-                image: 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                },
+                image: 'https://images.unsplash.com/flagged/photo-1557691844-0fc874166691?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2654&q=80'
+            },
                 {
-                    image: 'https://images.unsplash.com/photo-1530521954074-e64f6810b32d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80'
-                    } 
+                    image: 'https://images.unsplash.com/photo-1606391901318-07003db08d63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+                 }, 
 ]
